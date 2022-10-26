6078fb4f38f444aeca98194dc388552b3edf1248
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-10-26 10:54:47 -0600

    public javascript

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector("#txtEmail").value.trim();
+    const password = document.querySelector("#txtPassword").value.trim();
+  
+    if (email && password) {
+      // trying to login
+      const response = await fetch("/api/users/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/");
+      } else {
+        alert("Failed to log in");
+      }
+    }
+  };
+  
+  document
+    .querySelector("#loginForm")
+    .addEventListener("submit", loginFormHandler);
